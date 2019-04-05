a2c964e6f7eb470e5b528ec402204705a371d961
JuliaGavrilenko <yulya952@rambler.ru>
2019-04-05 18:20:58 +0200

    fix init.sh

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -10,5 +10,6 @@ source ~/.bashrc
 mkdir -p $SPARK_WORKER_LOG
 #ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 echo "Starting spark worker"
-nohup /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
+trap '' HUP
+/spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
     --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out 2>&1 </dev/null &
